3e47408428da181546469bd16a962d5409795eca
OasiBoi <ostally001@gmail.com>
2022-06-07 16:21:27 -0700

    added no_slug route

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 import static.components.wae as wae
 app = Flask(__name__)
 
+@app.route("/")
+def no_slug():
+    return wae.output.load_index_page()
+
 @app.route("/<slug>")
 def main(slug):
     return wae.output.load_index_page()
